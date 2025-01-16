983109e6059bc47bb94939f9f3c889ab6be7ba35
Marco Garzola <mgarzola@tecniplast.it>
2025-01-16 13:01:53 +0100

    - add metrics utils - add different yolo impl - add evaluate on base yolo class

--- saltup/ai/object_detection/yolo/yolo_factory.py
+++ saltup/ai/object_detection/yolo/yolo_factory.py
@@ -1,25 +1,21 @@
 from enum import IntEnum
 from saltup.ai.object_detection.yolo import BaseYolo, YoloType
-
+from saltup.ai.object_detection.yolo.impl import YoloAnchorsBased ,YoloUltralitics, YoloNas, YoloDamo
 
 class YoloFactory:
-	@staticmethod
-	def create(yolo_type: YoloType) -> BaseYolo:
-		if yolo_type == YoloType.ANCHORS_BASED:
-			# from saltup.ai.object_detection.preprocessing.impl import AnchorsBasedPreprocess
-			# return AnchorsBasedPreprocess()
-			pass
-		elif yolo_type == YoloType.ULTRALYTICS:
-			# from saltup.ai.object_detection.preprocessing.impl import UltralyticsPreprocess
-			# return UltralyticsPreprocess()
-			pass
-		elif yolo_type == YoloType.SUPERGRAD:
-			#  from saltup.ai.object_detection.preprocessing.impl import SupergradPreprocess
-			#  return SupergradPreprocess()
-			pass
-		elif yolo_type == YoloType.DAMO:
-			#  from saltup.ai.object_detection.preprocessing.impl import DamoPreprocessing
-			#  return DamoPreprocessing()
-			pass
-		else:
-			raise ValueError(f"Unknown processor type: {yolo_type}")
+    @staticmethod
+    def create(yolo_type: YoloType, modelpath: str, **kwargs) -> BaseYolo:
+        if yolo_type == YoloType.ANCHORS_BASED:
+            from saltup.ai.object_detection.preprocessing.impl import AnchorsBasedPreprocess
+            return YoloAnchorsBased(yolo_type, modelpath, kwargs['anchors'])
+        elif yolo_type == YoloType.ULTRALYTICS:
+            from saltup.ai.object_detection.preprocessing.impl import UltralyticsPreprocess
+            return YoloUltralitics(modelpath, **kwargs)
+        elif yolo_type == YoloType.SUPERGRAD:
+            from saltup.ai.object_detection.preprocessing.impl import SupergradPreprocess
+            return YoloNas(modelpath, **kwargs)
+        elif yolo_type == YoloType.DAMO:
+            from saltup.ai.object_detection.preprocessing.impl import DamoPreprocessing
+            return YoloDamo(modelpath, **kwargs)
+        else:
+            raise ValueError(f"Unknown processor type: {yolo_type}")
